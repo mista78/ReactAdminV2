ead08f04807e2638544ee7491d5352cc19385d6f
Kmaoulida <kmaoulida@partenaire-intersport.fr>
2023-03-31 11:21:06 +0200

    Add Helpers EditorSetting

--- src/Pages/Editor/Helpers/index.js
+++ src/Pages/Editor/Helpers/index.js
@@ -2,10 +2,11 @@ import BorderRadius from './BorderRadius';
 import Spaces from './Spaces';
 import Background from './Background';
 import References from './References';
-
+import EditorSeting from './EditorSetting';
 export {
     BorderRadius,
     Spaces,
     References,
+    EditorSeting,
     Background,
 }
