460b4538e11b1a8816b09182ade9fd29d9c32c38
Kmaoulida <kmaoulida@partenaire-intersport.fr>
2023-04-04 07:26:12 +0200

    Merge

--- src/Pages/Editor/Helpers/AddComponent/index.js
+++ src/Pages/Editor/Helpers/AddComponent/index.js
@@ -3,6 +3,7 @@ import { AppContext } from '../../../../store';
 import updatePropertyById from '../../../../Utils/updatePropertyById';
 import { kebabize, uuid } from '../../../../Utils/tools';
 
+<<<<<<< HEAD
 import styled from 'styled-components';
 
 const Lines = styled.div`
@@ -19,6 +20,12 @@ const Lines = styled.div`
 const AddComponent = ({ data, AllComponent, children }) => {
     const { state, dispatch } = useContext(AppContext);
     const handleAddComponent = (value) => {
+=======
+const AddComponent = ({ data, AllComponent, children }) => {
+    const { state, dispatch } = useContext(AppContext);
+    const handleAddComponent = (e) => {
+        const value = e ? e.target.value : 'Line';
+>>>>>>> 89236d8 (Merge && fix conflit)
         const newBlock = {
             id: uuid(),
             name: value,
@@ -28,6 +35,7 @@ const AddComponent = ({ data, AllComponent, children }) => {
         }
         const components = state.components.map(item => updatePropertyById(data.id, item, 'children', [...data.children, newBlock]));
         dispatch({ type: "ADD_COMPONENT", components });
+<<<<<<< HEAD
     };
     return <Fragment>
         <div>Setting : {data.id}</div>
@@ -36,6 +44,18 @@ const AddComponent = ({ data, AllComponent, children }) => {
                 return <div key={index} onClick={e => handleAddComponent(item)}>{item}</div>
             })}
         </Lines>
+=======
+        e && (e.target.value = '');
+    };
+    return <Fragment>
+        <div>Setting : {data.id}</div>
+        <select onChange={handleAddComponent}>
+            <option value="">Select Component</option>
+            {Object.keys(AllComponent).map((item, index) => {
+                return <option key={index} value={item}>{item}</option>
+            })}
+        </select>
+>>>>>>> 89236d8 (Merge && fix conflit)
     </Fragment>
 }
 
--- src/Pages/Editor/index.js
+++ src/Pages/Editor/index.js
@@ -263,15 +263,12 @@ const Editor = () => {
 
                     <div className="left">
                         <div id="sidebar">
-<<<<<<< HEAD
-                            <View data={state.components} type="setting" setting={true} />
-=======
                             <View data={state.components} setting={true} />
 
                             <button onClick={e => {
                                 localStorage.clear()
                             }}> Remove Storage </button>
->>>>>>> 68696ca (addComponent HeroBanner)
+
                         </div>
                     </div>
                     <div className="center">
