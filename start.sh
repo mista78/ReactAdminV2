54ad0997c5a9974c2c99db955b45eb2aefd12a89
Kmaoulida <kmaoulida@partenaire-intersport.fr>
2023-04-04 07:03:51 +0200

    Update script sh

--- start.sh
+++ start.sh
@@ -5,6 +5,12 @@ port="35729,3000"
 
 # find command node process and get the pid
 
+passwordSudo="your_password"
+
+# set the password for sudo
+echo $passwordSudo | sudo -S echo "sudo password set"
+
+
 for i in $(echo $port | sed "s/,/ /g")
 do
   pid=$(lsof -t -i:$i)
@@ -13,7 +19,8 @@ do
   # kill the process if it exists
   if [ -n "$pid" ]; then
     echo "killing process $pid"
-    sudo kill -9 $pid
+    sudo  kill -9 $pid
+
   fi
   lsof -t -i:$port
 done
